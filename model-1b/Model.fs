e7e431a9f10d011359bf2053b208663c52ae09ea
Bruno <ask@bruno.wtf>
2024-08-21 01:29:30 +0200

    fix model

--- model-1b/Model.fs
+++ model-1b/Model.fs
@@ -7,18 +7,26 @@ open SCM
 
 let i : Map<string, EndogenousVariable> = 
     Map.ofList [
-        ("H-longitude", {Type = typeof<float>; Equation = fun ctx -> 
-            let m_longitude = ctx.latest "M-longitude" :?> float
-            let epsilon = ctx.value "epsilon" :?> float
-            let lambda = ctx.value "lambda" :?> float
-            let t = ctx.t
-            m_longitude + epsilon * Math.Exp(-lambda * (t - (ctx.latest "M-longitude" :?> DateTime)).TotalSeconds) })
-        ("H-latitude", {Type = typeof<float>; Equation = fun ctx -> 
-            let m_latitude = ctx.latest "M-latitude" :?> float
-            let epsilon = ctx.value "epsilon" :?> float
-            let lambda = ctx.value "lambda" :?> float
-            let t = ctx.t
-            m_latitude + epsilon * Math.Exp(-lambda * (t - (ctx.latest "M-latitude" :?> DateTime)).TotalSeconds) })
+        ("H-longitude", {
+            Type = typeof<float>;
+            Equation = fun ctx -> 
+                let m_longitude = ctx.latest "M-longitude" :?> float
+                let epsilon = ctx.value "epsilon" :?> float
+                let lambda = ctx.value "lambda" :?> float
+                let t = ctx.t
+                let h_longitude = m_longitude + epsilon * Math.Exp(-lambda * (t - (ctx.latestTime "M-longitude")).TotalSeconds)
+                h_longitude
+        })
+        ("H-latitude", {
+            Type = typeof<float>;
+            Equation = fun ctx -> 
+                let m_latitude = ctx.latest "M-latitude" :?> float
+                let epsilon = ctx.value "epsilon" :?> float
+                let lambda = ctx.value "lambda" :?> float
+                let t = ctx.t
+                let h_latitude = m_latitude + epsilon * Math.Exp(-lambda * (t - (ctx.latestTime "M-latitude")).TotalSeconds)
+                h_latitude
+         })
         ("epsilon", {Type = typeof<float>; Equation = fun _ -> 0.5})
         ("lambda", {Type = typeof<float>; Equation = fun _ -> 0.05})
     ]
@@ -41,11 +49,3 @@ let integrateObservation (j: J) (obs: ObservationTypes) =
                        |> addMeasurement "M-longitude" googleObs.Timestamp googleObs.Longitude
                        |> addMeasurement "M-latitude" googleObs.Timestamp googleObs.Latitude
         updatedJ
-
-///// PROBABILITY MODEL /////
-
-let calculateProbability (expected: Position) (observed: Position) (lambda: float) : float =
-    let (Latitude lat1, Longitude lon1) = expected
-    let (Latitude lat2, Longitude lon2) = observed
-    let distance = Math.Sqrt((lat1 - lat2) ** 2.0 + (lon1 - lon2) ** 2.0)
-    Math.Exp(-lambda * distance)
