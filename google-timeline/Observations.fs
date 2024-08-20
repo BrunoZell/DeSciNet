e04ea77609788c4a1c4de2488039ab477a0d3b1e
Bruno <ask@bruno.wtf>
2024-08-21 01:19:38 +0200

    namespace

--- google-timeline/Observations.fs
+++ google-timeline/Observations.fs
@@ -11,5 +11,5 @@ type GoogleLocationObservation = {
 
 // Load observations from JSON file
 let loadObservations (filePath: string) : GoogleLocationObservation list =
-    let json = File.ReadAllText(filePath)
+    let json = System.IO.File.ReadAllText(filePath)
     JsonSerializer.Deserialize<GoogleLocationObservation list>(json)
