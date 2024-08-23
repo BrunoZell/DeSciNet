58eb2826ac5f5defde944e3d271c2e8223fb5741
Bruno <ask@bruno.wtf>
2024-08-23 16:03:17 +0200

    disable timezone

--- .devcontainer/install-euclid-dependencies.sh
+++ .devcontainer/install-euclid-dependencies.sh
@@ -108,8 +108,8 @@ tar xz && \
 sudo mv ${YQ_BINARY} /usr/bin/yq
 
 # Install ansible
-echo 'Etc/UTC' | sudo tee /etc/timezone && \
-sudo ln -s /usr/share/zoneinfo/Etc/UTC /etc/localtime
+# echo 'Etc/UTC' | sudo tee /etc/timezone && \
+# sudo ln -s /usr/share/zoneinfo/Etc/UTC /etc/localtime
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y --update ppa:ansible/ansible
 sudo apt-get install -y ansible
