d0dfefb23bfa6d0e5c5ff0b53d7288737aefb338
Bruno <ask@bruno.wtf>
2024-08-27 16:23:29 +0200

    serializers

---
+++ metagraph/source/project/descinet/modules/shared_data/src/main/scala/com/my/nft/shared_data/serializers/Serializers.scala
@@ -0,0 +1,49 @@
+package com.descinet.shared_data.serializers
+
+import com.descinet.shared_data.types.Types._
+import io.circe.Encoder
+import io.circe.syntax.EncoderOps
+import org.tessellation.currency.dataApplication.DataUpdate
+import org.tessellation.currency.dataApplication.dataApplication.DataApplicationBlock
+import org.tessellation.security.signature.Signed
+
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+
+object Serializers {
+  private def serialize[A: Encoder](
+    serializableData: A
+  ): Array[Byte] = {
+    serializableData.asJson.deepDropNullValues.noSpaces.getBytes(StandardCharsets.UTF_8)
+  }
+
+  def serializeUpdate(
+    update: DeSciNetUpdate
+  ): Array[Byte] = {
+    val encoder = Base64.getEncoder
+    val data_sign_prefix = "\u0019Constellation Signed Data:\n"
+
+    val updateBytes = update.asJson.deepDropNullValues.noSpaces.getBytes(StandardCharsets.UTF_8)
+    val encodedBytes = encoder.encode(updateBytes)
+
+    val encodedString = new String(encodedBytes, "UTF-8")
+    val completeString = s"$data_sign_prefix${encodedString.length}\n$encodedString"
+
+    completeString.getBytes(StandardCharsets.UTF_8)
+  }
+
+  def serializeState(
+    state: DeSciNetOnChainState
+  ): Array[Byte] =
+    serialize[DeSciNetOnChainState](state)
+
+  def serializeBlock(
+    state: Signed[DataApplicationBlock]
+  )(implicit e: Encoder[DataUpdate]): Array[Byte] =
+    serialize[Signed[DataApplicationBlock]](state)
+
+  def serializeCalculatedState(
+    state: DeSciNetCalculatedState
+  ): Array[Byte] =
+    serialize[DeSciNetCalculatedState](state)
+}
