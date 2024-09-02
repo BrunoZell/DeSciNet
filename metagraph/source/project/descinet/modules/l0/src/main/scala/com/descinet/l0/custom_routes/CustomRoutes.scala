da62d5fdd30ade607267e3559e0958e6aeed9519
Bruno <ask@bruno.wtf>
2024-09-02 14:59:13 +0200

    disable custom routes

--- metagraph/source/project/descinet/modules/l0/src/main/scala/com/descinet/l0/custom_routes/CustomRoutes.scala
+++ metagraph/source/project/descinet/modules/l0/src/main/scala/com/descinet/l0/custom_routes/CustomRoutes.scala
@@ -7,6 +7,7 @@ import com.descinet.shared_data.calculated_state.CalculatedStateService
 import com.descinet.shared_data.types.Types._
 import eu.timepit.refined.auto._
 import org.http4s._
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.middleware.CORS
 import org.tessellation.ext.http4s.AddressVar
@@ -19,90 +20,90 @@ case class CustomRoutes[F[_] : Async](calculatedStateService: CalculatedStateSer
 
   private def getAllModels: F[Response[F]] = {
     getState.flatMap { state =>
-      val allModelsResponse = state.models.map { case (_, model) => formatToModelResponse(model) }.toList
+      val allModelsResponse = state.models.toList
       Ok(allModelsResponse)
     }
   }
 
-  private def getAllModelsGroupedByTarget: F[Response[F]] = {
-    getState.flatMap { state =>
-      val groupedModels = state.models.groupBy(_._2.targetVariableGroup).map {
-        case (group, models) => group -> models.sortBy(-_._2.totalSurprise)
-      }
-      Ok(groupedModels)
-    }
-  }
+  // private def getAllModelsGroupedByTarget: F[Response[F]] = {
+  //   getState.flatMap { state =>
+  //     val groupedModels = state.models.groupBy(_._2.targetVariableGroup).map {
+  //       case (group, models) => group -> models.sortBy(-_._2.totalSurprise)
+  //     }
+  //     Ok(groupedModels)
+  //   }
+  // }
 
   private def getAllModelsByAddress(
     address: Address
   ): F[Response[F]] = {
     getState.flatMap { state =>
-      val addressModels = state.models.filter { model =>
-        model._2.owner == address
+      val addressModels = state.models.filter { case (_, model) =>
+        model.author == address
       }
       Ok(addressModels)
     }
   }
 
-  private def getModelEquationsById(
-    modelId: String
-  ): F[Response[F]] = {
-    getState.flatMap { state =>
-      state.models.get(modelId.toLong).map { model =>
-        Ok(model.equations)
-      }.getOrElse(NotFound())
-    }
-  }
+  // private def getModelEquationsById(
+  //   modelId: String
+  // ): F[Response[F]] = {
+  //   getState.flatMap { state =>
+  //     state.models.get(modelId.toLong).map { model =>
+  //       Ok(model.equations)
+  //     }.getOrElse(NotFound())
+  //   }
+  // }
 
-  private def getTotalSurpriseGraph(
-    modelId: String
-  ): F[Response[F]] = {
-    getState.flatMap { state =>
-      state.models.get(modelId.toLong).map { model =>
-        val totalSurpriseData = model.backtest.map { measurement =>
-          measurement.timestamp -> measurement.totalSurprise
-        }
-        Ok(totalSurpriseData)
-      }.getOrElse(NotFound())
-    }
-  }
+  // private def getTotalSurpriseGraph(
+  //   modelId: String
+  // ): F[Response[F]] = {
+  //   getState.flatMap { state =>
+  //     state.models.get(modelId.toLong).map { model =>
+  //       val totalSurpriseData = model.backtest.map { measurement =>
+  //         measurement.timestamp -> measurement.totalSurprise
+  //       }
+  //       Ok(totalSurpriseData)
+  //     }.getOrElse(NotFound())
+  //   }
+  // }
 
-  private def getConsensusSurpriseGraph(
-    targetId: String
-  ): F[Response[F]] = {
-    getState.flatMap { state =>
-      val consensusSurpriseData = state.globalModelBacktest.filter(_.targetId == targetId).map { measurement =>
-        measurement.timestamp -> measurement.consensusSurprise
-      }
-      Ok(consensusSurpriseData)
-    }
-  }
+  // private def getConsensusSurpriseGraph(
+  //   targetId: String
+  // ): F[Response[F]] = {
+  //   getState.flatMap { state =>
+  //     val consensusSurpriseData = state.globalModelBacktest.filter(_.targetId == targetId).map { measurement =>
+  //       measurement.timestamp -> measurement.consensusSurprise
+  //     }
+  //     Ok(consensusSurpriseData)
+  //   }
+  // }
 
-  private def getAllVariablesGroupedByTargets: F[Response[F]] = {
-    getState.flatMap { state =>
-      val groupedVariables = state.variables.groupBy(_.target)
-      Ok(groupedVariables)
-    }
-  }
+  // private def getAllVariablesGroupedByTargets: F[Response[F]] = {
+  //   getState.flatMap { state =>
+  //     val groupedVariables = state.variables.groupBy(_.target)
+  //     Ok(groupedVariables)
+  //   }
+  // }
 
-  private def rankContributorsByRewards: F[Response[F]] = {
-    getState.flatMap { state =>
-      val rankedContributors = state.contributors.map { contributor =>
-        contributor.id -> (contributor.rewardsFromModel + contributor.rewardsFromSampling)
-      }.toList.sortBy(-_._2)
-      Ok(rankedContributors)
-    }
-  }
+  // private def rankContributorsByRewards: F[Response[F]] = {
+  //   getState.flatMap { state =>
+  //     val rankedContributors = state.contributors.map { contributor =>
+  //       contributor.id -> (contributor.rewardsFromModel + contributor.rewardsFromSampling)
+  //     }.toList.sortBy(-_._2)
+  //     Ok(rankedContributors)
+  //   }
+  // }
 
   private val routes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "models" => getAllModels
-    case GET -> Root / "models" / "grouped-by-target" => getAllModelsGroupedByTarget
+    // case GET -> Root / "models" / "grouped-by-target" => getAllModelsGroupedByTarget
     case GET -> Root / "models" / AddressVar(address) => getAllModelsByAddress(address)
-    case GET -> Root / "models" / modelId / "equations" => getModelEquationsById(modelId)
-    case GET -> Root / "models" / modelId / "total-surprise-graph" => getTotalSurpriseGraph(modelId)
-    case GET -> Root / "targets" / targetId / "consensus-surprise-graph" => getConsensusSurpriseGraph(targetId)
-    case GET -> Root / "variables" / "grouped-by-targets" => getAllVariablesGroupedByTargets
-    case GET -> Root / "contributors" / "ranked-by-rewards" => rankContributorsByRewards
+    // case GET -> Root / "models" / modelId / "equations" => getModelEquationsById(modelId)
+    // case GET -> Root / "models" / modelId / "total-surprise-graph" => getTotalSurpriseGraph(modelId)
+    // case GET -> Root / "targets" / targetId / "consensus-surprise-graph" => getConsensusSurpriseGraph(targetId)
+    // case GET -> Root / "variables" / "grouped-by-targets" => getAllVariablesGroupedByTargets
+    // case GET -> Root / "contributors" / "ranked-by-rewards" => rankContributorsByRewards
   }
 
   val public: HttpRoutes[F] =
