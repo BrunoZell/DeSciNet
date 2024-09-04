824ef847e4fec86f3da1f04735eae9c9c1dc6900
Bruno <ask@bruno.wtf>
2024-09-04 21:20:35 +0200

    extract causal model evaluator into own file

---
+++ metagraph/source/project/descinet/modules/l0/src/main/scala/com/descinet/l0/CausalModelEvaluator.scala
@@ -0,0 +1,90 @@
+package com.descinet.l0
+
+import scala.reflect.runtime.universe._
+import scala.tools.reflect.ToolBox
+import scala.util.Random
+import com.descinet.shared_data.types.Types._
+
+class ModelEvaluator(
+  externalMeasurements: Map[String, List[Measurement]],
+  model: Model,
+  time: Long
+) {
+  private val rng = new Random()
+  private var evaluatedCache: Map[String, Double] = Map()
+  private var evaluationStack: Set[String] = Set()
+  private val toolbox = runtimeMirror(getClass.getClassLoader).mkToolBox()
+
+  // Helper functions for randomness
+  def randomDouble(): Double = rng.nextDouble()
+  def randomGaussian(): Double = rng.nextGaussian()
+
+  // Retrieves the latest observation value for a given external variable name at a specified time
+  def latest(exolabel: String, time: Long): Option[Double] = {
+    externalMeasurements.get(exolabel).flatMap { measurements =>
+      measurements.find(_.timestamp <= time).map(_.value)
+    }
+  }
+
+  // Retrieves the timestamp of the last observation for a given external variable name at a specified time
+  def latestTime(exolabel: String, time: Long): Option[Long] = {
+    externalMeasurements.get(exolabel).flatMap { measurements =>
+      measurements.find(_.timestamp <= time).map(_.timestamp)
+    }
+  }
+
+  // Evaluate an endogenous variable
+  def evaluateEndogenous(label: String, t: Long): Double = {
+    if (evaluationStack.contains(label)) {
+      throw new RuntimeException(s"Cyclic dependency detected for variable $label")
+    }
+
+    evaluatedCache.getOrElse(label, {
+      evaluationStack += label
+      val equation = model.internalVariables(model.internalParameterLabels(label)).equation
+      val result = evaluateEquation(equation, t, model.externalParameterLabels.keySet)
+      evaluatedCache += (label -> result)
+      evaluationStack -= label
+      result
+    })
+  }
+
+  // Parse and compile Scala code from string
+  def evaluateEquation(equation: String, t: Long, externalLabels: Set[String]): Double = {
+    val externalSymbols = externalLabels.map(label => s"val $label = \"$label\"").mkString("\n")
+    val endogenousSymbols = model.internalParameterLabels.keys.map { label =>
+      s"def $label(t: Long): Double = evaluateEndogenous(\"$label\", t)"
+    }.mkString("\n")
+
+    val code = s"""
+      |(env: Any) => {
+      |  import scala.math._
+      |  val randomDouble = () => env.randomDouble() 
+      |  val randomGaussian = () => env.randomGaussian() 
+      |  val t = $t 
+      |  $externalSymbols
+      |  $endogenousSymbols
+      |  $equation
+      |}
+    """.stripMargin
+
+    val tree = toolbox.parse(code)
+    val compiledCode = toolbox.compile(tree)
+    compiledCode()(this).asInstanceOf[Double]
+  }
+
+  // Main evaluation process
+  def evaluate(): Map[String, Double] = {
+    model.internalVariables.zipWithIndex.map { case (variable, idx) =>
+      val label = model.internalParameterLabels.collectFirst { case (key, `idx`) => key }.get
+      val samples = (1 to 1000).map { _ =>
+        evaluatedCache.getOrElse(label, {
+          val result = evaluateEquation(variable.equation, time, model.externalParameterLabels.keySet)
+          evaluatedCache += (label -> result)
+          result
+        })
+      }
+      label -> samples.sum / samples.size
+    }.toMap
+  }
+}
--- metagraph/source/project/descinet/modules/l0/src/main/scala/com/descinet/l0/custom_routes/CustomRoutes.scala
+++ metagraph/source/project/descinet/modules/l0/src/main/scala/com/descinet/l0/custom_routes/CustomRoutes.scala
@@ -3,6 +3,7 @@ package com.descinet.l0.custom_routes
 import cats.effect.Async
 import cats.syntax.flatMap.toFlatMapOps
 import cats.syntax.functor.toFunctorOps
+import com.descinet.l0.ModelEvaluator
 import com.descinet.shared_data.calculated_state.CalculatedStateService
 import com.descinet.shared_data.types.Types._
 import eu.timepit.refined.auto._
@@ -105,114 +106,8 @@ case class CustomRoutes[F[_] : Async](calculatedStateService: CalculatedStateSer
         case None => NotFound(s"Model with id $modelId not found.")
         case Some(model) =>
           val externalMeasurements = collectExternalMeasurementsForModel(state, model, time, hideLatest)
-          
-          // Log the resulting externalMeasurements
-          // println(s"External Measurements: $externalMeasurements")
-
-          // The EvaluationEnvironment contains all symbols available to structured equations.
-          class EvaluationEnvironment(externalMeasurements: Map[String, List[Measurement]], model: Model) {
-            private val rng = new Random()
-            private var evaluatedCache: Map[String, Double] = Map()
-            private var evaluationStack: Set[String] = Set()
-
-            // Helper functions for randomness
-            def randomDouble(): Double = rng.nextDouble()
-            def randomGaussian(): Double = rng.nextGaussian()
-
-            // New methods to conform to the required signatures
-
-            /**
-             * Retrieves the latest observation value (Double) for a given external variable name (exolabel) at a specified time (time).
-             * Assumes that the list of measurements for each exolabel is sorted in descending order by timestamp.
-             * This assumption holds because the lists are reversed in the collectExternalMeasurementsForModel method.
-             */
-            def latest(exolabel: String, time: Long): Option[Double] = {
-              externalMeasurements.get(exolabel).flatMap { measurements =>
-                // Find the first measurement with a timestamp <= time
-                measurements.find(_.timestamp <= time).map(_.value)
-              }
-            }
-
-            /**
-             * Retrieves the timestamp of the last observation for a given external variable name (exolabel) at a specified time (time).
-             * Assumes that the list of measurements for each exolabel is sorted in descending order by timestamp.
-             * This assumption holds because the lists are reversed in the collectExternalMeasurementsForModel method.
-             */
-            def latestTime(exolabel: String, time: Long): Option[Long] = {
-              externalMeasurements.get(exolabel).flatMap { measurements =>
-                // Find the first measurement with a timestamp <= time
-                measurements.find(_.timestamp <= time).map(_.timestamp)
-              }
-            }
-
-            // Evaluate an endogenous variable
-            def evaluateEndogenous(label: String, t: Long): Double = {
-              if (evaluationStack.contains(label)) {
-                throw new RuntimeException(s"Cyclic dependency detected for variable $label")
-              }
-
-              evaluatedCache.getOrElse(label, {
-                evaluationStack += label
-                val equation = model.internalVariables(model.internalParameterLabels(label)).equation
-                val result = evaluateEquation(equation, t, model.externalParameterLabels.keySet)
-                evaluatedCache += (label -> result)
-                evaluationStack -= label
-                result
-              })
-            }
-
-            // Parse and compile Scala code from string
-            def evaluateEquation(equation: String, t: Long, externalLabels: Set[String]): Double = {
-              // Generate Scala code to define symbols for each external label
-              val externalSymbols = externalLabels.map(label => s"val $label = \"$label\"").mkString("\n")
-
-              // Generate Scala code to define symbols for each endogenous variable
-              val endogenousSymbols = model.internalParameterLabels.keys.map { label =>
-                s"def $label(t: Long): Double = env.evaluateEndogenous(\"$label\", t)"
-              }.mkString("\n")
-
-              val code = s"""
-                |(env: Any) => {
-                |  import scala.math.{abs, acos, asin, atan, atan2, cbrt, ceil, cos, cosh, exp, floor, hypot, log, log10, max, min, pow, round, signum, sin, sinh, sqrt, tan, tanh}
-                |  val randomDouble = () => env.randomDouble() 
-                |  val randomGaussian = () => env.randomGaussian() 
-                |  val t = $t 
-                |  $externalSymbols
-                |  $endogenousSymbols
-                |  $equation
-                |}
-              """.stripMargin
-
-              val tree = toolbox.parse(code)
-              val compiledCode = toolbox.compile(tree)
-              compiledCode()(env).asInstanceOf[Double]
-            }
-          }
-
-          val env = new EvaluationEnvironment(externalMeasurements, model)
-
-          // Cache for storing evaluated endogenous variables
-          var evaluatedCache: Map[String, Double] = Map()
-
-          // Main evaluation process
-          val results = model.internalVariables.zipWithIndex.map { case (variable, idx) =>
-            val label = model.internalParameterLabels.collectFirst { case (key, `idx`) => key }.get
-            val samples = (1 to 1000).map { _ =>
-              val cached = evaluatedCache.get(label)
-              cached match {
-                case Some(value) => value
-                case None =>
-                  // Evaluate the endogenous variable using the Scala code
-                  val result = env.evaluateEquation(variable.equation, time, model.externalParameterLabels.keySet)
-                  evaluatedCache += (label -> result)
-                  result
-              }
-            }
-            // Return the average sample value
-            label -> samples.sum / samples.size
-          }
-
-          // Return the computed response as JSON
+          val evaluator = new ModelEvaluator(externalMeasurements, model, time)
+          val results = evaluator.evaluate()
           Ok(results)
       }
     }
